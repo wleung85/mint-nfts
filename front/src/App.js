d97ae77ac62b0fff430a2646a288ce8d38715e11
Winston Leung <wleung85@uwo.ca>
2021-10-16 22:04:26 -0400

    Added buttons and hooks on the frontend to display the number of minted tokens so far

--- front/src/App.js
+++ front/src/App.js
@@ -7,13 +7,24 @@ import myEpicNft from './utils/MyEpicNFT.json';
 // Constants
 const TWITTER_HANDLE = 'wl_devs';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
-const OPENSEA_LINK = '';
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/squarenft-mrty74bp3f';
 const TOTAL_MINT_COUNT = 50;
-const CONTRACT_ADDRESS = "0x8f1bd36C9537B7EeE872730580fa41E7b4dAfcB9";
+const CONTRACT_ADDRESS = "0xeA1BF00f718452ea90497BD33c5A696DeF6700B3";
+const RINKEBY_CHAINID = "0x4";
 
 const App = () => {
 
   const [currentAccount, setCurrentAccount] = useState("");
+  const [mintedCount, _setMintedCount] = useState(0);
+  const [isMinting, setIsMinting] = useState(false);
+  const [latestMint, setLatestMint] = useState("");
+
+  const mintedCountRef = React.useRef(mintedCount);
+
+  const setMintedCount = x => {
+    mintedCountRef.current = x;
+    _setMintedCount(x);
+  }
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window;
@@ -26,6 +37,11 @@ const App = () => {
     }
 
     const accounts = await ethereum.request({ method: 'eth_accounts' });
+    let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+      if (chainId !== RINKEBY_CHAINID) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
 
     if (accounts.length !== 0) {
       const account = accounts[0];
@@ -50,6 +66,11 @@ const App = () => {
       }
 
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+      let chainId = await ethereum.request({ method: 'eth_chainId' });
+      console.log("Connected to chain " + chainId);
+      if (chainId !== RINKEBY_CHAINID) {
+        alert("You are not connected to the Rinkeby Test Network!");
+      }
 
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
@@ -71,10 +92,15 @@ const App = () => {
         const signer = provider.getSigner();
         const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
+        let count = await connectedContract.getCurrMinted();
+
+        setMintedCount(count.toNumber());
+
         // This will essentially "capture" our event when our contract throws it.
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber())
-          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on Rarible. Here's the link: https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`)
+          setLatestMint(`https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`);
+          setMintedCount(mintedCountRef.current + 1);
         });
 
         console.log("Setup event listener!")
@@ -99,9 +125,11 @@ const App = () => {
         console.log("Going to pop wallet now to pay gas...");
         let nftTxn = await connectedContract.makeAnEpicNFT();
 
+        setIsMinting(true);
         console.log("Mining...please wait.");
         await nftTxn.wait();
 
+        setIsMinting(false);
         console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
       } else {
         console.log("Ethereum object doesn't exist!");
@@ -128,10 +156,34 @@ const App = () => {
               Connect to Wallet
             </button>
           ) : (
-            <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+            <>
+            <p className="sub-text" style={{'marginTop': 0, 'marginBottom': 0}}>
+              Minted so far:
+            </p>
+            <p className="header gradient-text">
+            {mintedCount} / {TOTAL_MINT_COUNT}
+            </p>
+            <div style={{"height": "120px", "display": "flex", "alignItems": "center", "justifyContent": "center"}}>
+            {isMinting ? 
+              <>
+              <p className="sub-text">Minting...</p><br/>
+              <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+              </>
+              : 
+              <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
               Mint NFT
+              </button>}
+            </div>
+            <br/>
+            <button onClick={() => {window.location = OPENSEA_LINK}} className="cta-button connect-wallet-button">
+            🌊 View Collection on OpenSea
             </button>
+            <br/>
+            <br/>
+            </>
           )}
+          {latestMint === "" ? <></> :
+          <button onClick={() => {window.location = latestMint}} className="cta-button connect-wallet-button">Most recently minted NFT</button>}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
